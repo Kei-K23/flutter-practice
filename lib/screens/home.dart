0367bf3e281db42c796c7113bdf42e837f2d4c02
Kei-K23 <keiksl2301@gmail.com>
2025-06-01 16:58:35 +0630

    feat: add home screen header widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,6 +5,73 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(children: [header(), SizedBox(height: 30), category()]),
+    );
+  }
+
+  Column category() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [Text("Categories")],
+    );
+  }
+
+  Container header() {
+    return Container(
+      color: Colors.blue.shade200,
+      width: double.infinity,
+      height: 350,
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Hello, Gojo 👋🏻",
+                style: const TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white.withValues(alpha: 0.3),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Icon(Icons.notifications, color: Colors.white),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          const Text(
+            "Let find\nyour top doctor",
+            style: TextStyle(
+              fontSize: 36,
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 20),
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Search...",
+              hintStyle: const TextStyle(fontWeight: FontWeight.w300),
+              prefixIcon: const Icon(Icons.search, size: 25),
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(50),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
